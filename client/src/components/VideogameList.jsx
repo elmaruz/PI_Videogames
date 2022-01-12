fa9ee7e3909e463495a1b1af86f8e1a3cc38c178
elmaruz <lmarussig@gmail.com>
2022-01-12 10:45:02 +0000

    12.01 - Validation fix VideogameList

--- client/src/components/VideogameList.jsx
+++ client/src/components/VideogameList.jsx
@@ -1,31 +1,32 @@
-import Videogame from "./Videogame.jsx";
-import styles from "../css_modules/VideogameList.module.css";
+import Videogame from './Videogame.jsx';
+import styles from '../css_modules/VideogameList.module.css';
 
 export default function VideogameList({ vids, vars }) {
   return (
     <div className={`${styles.macro}`}>
       <div className={`${styles.container}`}>
-        {vids.slice(vars[0], vars[1]).map((elem, index) => {
-          if (elem.name !== "Videogame not found") {
-            return (
-              <Videogame
-                key={index}
-                id={elem.id}
-                name={elem.name}
-                image={elem.image}
-                genres={elem.genres}
-                rating={elem.rating}
-              />
-            );
-          } else {
-            return (
-              <div>
-                <h1 className={`${styles.error}`}>GAME OVER</h1>
-                <h3 className={`${styles.error}`}>videogame not found</h3>
-              </div>
-            );
-          }
-        })}
+        {vids &&
+          vids.slice(vars[0], vars[1]).map((elem, index) => {
+            if (elem.name !== 'Videogame not found') {
+              return (
+                <Videogame
+                  key={index}
+                  id={elem.id}
+                  name={elem.name}
+                  image={elem.image}
+                  genres={elem.genres}
+                  rating={elem.rating}
+                />
+              );
+            } else {
+              return (
+                <div>
+                  <h1 className={`${styles.error}`}>GAME OVER</h1>
+                  <h3 className={`${styles.error}`}>videogame not found</h3>
+                </div>
+              );
+            }
+          })}
       </div>
     </div>
   );
