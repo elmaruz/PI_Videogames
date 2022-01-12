a135133061c25c70b96045abcb9078c9313d849b
elmaruz <lmarussig@gmail.com>
2022-01-12 10:48:38 +0000

    12.01 - FilterGen validation fix

--- client/src/components/FilterGen.jsx
+++ client/src/components/FilterGen.jsx
@@ -1,9 +1,9 @@
-import { useSelector } from "react-redux";
-import { useState } from "react";
-import styles from "../css_modules/GenDropdown.module.css";
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import styles from '../css_modules/GenDropdown.module.css';
 
 export default function FilterGen({ onFilterGen, genre }) {
-  let [value, setValue] = useState("");
+  let [value, setValue] = useState('');
   let genres = useSelector((state) => state.genres);
 
   function onChangeHandler(e) {
@@ -14,24 +14,23 @@ export default function FilterGen({ onFilterGen, genre }) {
   return (
     <div className={`${styles.drop}`}>
       <button
-        className={`${value && genre ? styles.menu_active : styles.menu}`}
-      >
-        {value && genre ? value : "Select Genre"}
+        className={`${value && genre ? styles.menu_active : styles.menu}`}>
+        {value && genre ? value : 'Select Genre'}
       </button>
       <div className={`${styles.select}`}>
-        <button className={`${styles.btn}`} value="" onClick={onChangeHandler}>
+        <button className={`${styles.btn}`} value='' onClick={onChangeHandler}>
           All Genres
         </button>
-        {genres.map((elem, index) => (
-          <button
-            className={`${styles.btn}`}
-            key={elem + index}
-            value={elem.name}
-            onClick={onChangeHandler}
-          >
-            {elem.name}
-          </button>
-        ))}
+        {genres &&
+          genres.map((elem, index) => (
+            <button
+              className={`${styles.btn}`}
+              key={elem + index}
+              value={elem.name}
+              onClick={onChangeHandler}>
+              {elem.name}
+            </button>
+          ))}
       </div>
     </div>
   );
