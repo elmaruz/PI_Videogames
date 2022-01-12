4573c9e0e7551e60dccf1d93e760872a48142dba
elmaruz <lmarussig@gmail.com>
2022-01-12 11:14:05 +0000

    12.01 - Possible fix for apikey

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
 const { Router } = require('express');
 const { Videogame, Genre } = require('../db.js');
 const axios = require('axios');
@@ -12,159 +14,161 @@ const router = Router();
 // Ejemplo: router.use('/auth', authRouter);
 
 router.get('/videogames', async (req, res) => {
-	let count = 0; // variable to keep api page count
-	let pages = []; // array to save api pages
-	let arr1 = []; // arr1 used to store games
-	if (!req.query.name) {
-		arr1 = []; // clearing arr1 before each request
-		let head = await axios.get(`https://rawg.io/api/games?key=${APIKEY}`);
-		let current = head.data; // api data structured as a linked list - assigning head to varible current
-		pages.push(current); // pushing head's content (full page) to pages array - each page contains 20 games
-		while (count < 4) {
-			let getNext = await axios.get(current.next); // grab four more pages going through this linked list
-			pages.push(getNext.data);
-			current = getNext.data;
-			count++;
-		}
-		let myDb = await Videogame.findAll({
-			include: Genre,
-		});
-		arr1.push(myDb); // if no req query, add all games from our db
-		for (var i = 0; i < pages.length; i++) {
-			var x = pages[i].results.map(elem => {
-				// needed properties come under the results prop of every page in api
-				return {
-					// mapping those properties out in an obj, reflecting how our db model structure for conformity
-					id: elem.id,
-					name: elem.name,
-					genres: elem.genres,
-					image: elem.background_image,
-					rating: elem.rating,
-					platforms: elem.platforms.map(elem => elem.platform.name),
-				};
-			});
-			arr1.push(x); // push every obj
-		}
-	} else if (req.query.name) {
-		try {
-			arr1 = [];
-			let search = await axios.get(
-				`https://rawg.io/api/games?key=${APIKEY}&search=${req.query.name}`
-			);
-			let curr = search.data; // api data structured as a linked list - assigning head to varible current
-			pages.push(curr); // pushing head's content (full page) to pages array - each page contains 20 games
-			while (count < 4) {
-				let findNext = await axios.get(curr.next); // grab four more pages going through this linked list
-				pages.push(findNext.data);
-				curr = findNext.data;
-				count++;
-			}
-			let findDb = await Videogame.findAll({
-				// if req query, find all games in db which contain req.query in name
-				include: Genre,
-				where: {
-					name: { [Op.iLike]: `%${req.query.name}%` },
-				},
-			});
-			arr1.push(findDb);
-			for (var i = 0; i < pages.length; i++) {
-				// filter out games in api pages which contain req query name
-				var y = pages[i].results
-					.filter(elem =>
-						elem.name.toLowerCase().includes(req.query.name.toLowerCase())
-					)
-					.map(elem => {
-						return {
-							id: elem.id,
-							name: elem.name,
-							genres: elem.genres,
-							image: elem.background_image,
-							rating: elem.rating,
-							platforms: elem.platforms.map(elem => elem.platform.name),
-						};
-					});
-				arr1.push(y); // push every obj which contains req.query in its name prop
-			}
-		} catch (error) {
-			arr1.push([{ name: 'Videogame not found', image: 'x', genres: [''] }]);
-		}
-	}
-	let flatarr = arr1.flat(); // flatten the array to get rid of nested arrays
-	res.send(flatarr);
+  let count = 0; // variable to keep api page count
+  let pages = []; // array to save api pages
+  let arr1 = []; // arr1 used to store games
+  if (!req.query.name) {
+    arr1 = []; // clearing arr1 before each request
+    let head = await axios.get(`https://rawg.io/api/games?key=${APIKEY}`);
+    let current = head.data; // api data structured as a linked list - assigning head to varible current
+    pages.push(current); // pushing head's content (full page) to pages array - each page contains 20 games
+    while (count < 4) {
+      let getNext = await axios.get(current.next); // grab four more pages going through this linked list
+      pages.push(getNext.data);
+      current = getNext.data;
+      count++;
+    }
+    let myDb = await Videogame.findAll({
+      include: Genre,
+    });
+    arr1.push(myDb); // if no req query, add all games from our db
+    for (var i = 0; i < pages.length; i++) {
+      var x = pages[i].results.map((elem) => {
+        // needed properties come under the results prop of every page in api
+        return {
+          // mapping those properties out in an obj, reflecting how our db model structure for conformity
+          id: elem.id,
+          name: elem.name,
+          genres: elem.genres,
+          image: elem.background_image,
+          rating: elem.rating,
+          platforms: elem.platforms.map((elem) => elem.platform.name),
+        };
+      });
+      arr1.push(x); // push every obj
+    }
+  } else if (req.query.name) {
+    try {
+      arr1 = [];
+      let search = await axios.get(
+        `https://rawg.io/api/games?key=${APIKEY}&search=${req.query.name}`
+      );
+      let curr = search.data; // api data structured as a linked list - assigning head to varible current
+      pages.push(curr); // pushing head's content (full page) to pages array - each page contains 20 games
+      while (count < 4) {
+        let findNext = await axios.get(curr.next); // grab four more pages going through this linked list
+        pages.push(findNext.data);
+        curr = findNext.data;
+        count++;
+      }
+      let findDb = await Videogame.findAll({
+        // if req query, find all games in db which contain req.query in name
+        include: Genre,
+        where: {
+          name: { [Op.iLike]: `%${req.query.name}%` },
+        },
+      });
+      arr1.push(findDb);
+      for (var i = 0; i < pages.length; i++) {
+        // filter out games in api pages which contain req query name
+        var y = pages[i].results
+          .filter((elem) =>
+            elem.name.toLowerCase().includes(req.query.name.toLowerCase())
+          )
+          .map((elem) => {
+            return {
+              id: elem.id,
+              name: elem.name,
+              genres: elem.genres,
+              image: elem.background_image,
+              rating: elem.rating,
+              platforms: elem.platforms.map((elem) => elem.platform.name),
+            };
+          });
+        arr1.push(y); // push every obj which contains req.query in its name prop
+      }
+    } catch (error) {
+      arr1.push([{ name: 'Videogame not found', image: 'x', genres: [''] }]);
+    }
+  }
+  let flatarr = arr1.flat(); // flatten the array to get rid of nested arrays
+  res.send(flatarr);
 });
 
 router.get('/videogame/:id', (req, res) => {
-	let id = req.params.id;
-	if (id && id.length < 9) {
-		axios
-			.get(`https://rawg.io/api/games/${id}?key=${APIKEY}`)
-			.then(r => r.data)
-			.then(data => {
-				let info = {
-					name: data.name,
-					genres: data.genres.map(elem => elem.name).join(' / '),
-					image: data.background_image,
-					description: data.description.replace(/(<([^>]+)>)/gi, ''),
-					released: data.released,
-					rating: data.rating,
-					platforms: data.platforms.map(elem => elem.platform.name).join(' - '),
-				};
-				res.send(info);
-			});
-	} else if (id && id.length > 9) {
-		Videogame.findByPk(id, { include: Genre }).then(r => {
-			let infoDb = {
-				// creating new obj with response values
-				name: r.name,
-				genres: r.genres.map(elem => elem.name).join(' / '),
-				image: r.image,
-				description: r.description,
-				released: r.released,
-				rating: r.rating,
-				platforms: r.platforms.map(elem => elem).join(' - '),
-			};
-			res.send(infoDb);
-		});
-	}
+  let id = req.params.id;
+  if (id && id.length < 9) {
+    axios
+      .get(`https://rawg.io/api/games/${id}?key=${APIKEY}`)
+      .then((r) => r.data)
+      .then((data) => {
+        let info = {
+          name: data.name,
+          genres: data.genres.map((elem) => elem.name).join(' / '),
+          image: data.background_image,
+          description: data.description.replace(/(<([^>]+)>)/gi, ''),
+          released: data.released,
+          rating: data.rating,
+          platforms: data.platforms
+            .map((elem) => elem.platform.name)
+            .join(' - '),
+        };
+        res.send(info);
+      });
+  } else if (id && id.length > 9) {
+    Videogame.findByPk(id, { include: Genre }).then((r) => {
+      let infoDb = {
+        // creating new obj with response values
+        name: r.name,
+        genres: r.genres.map((elem) => elem.name).join(' / '),
+        image: r.image,
+        description: r.description,
+        released: r.released,
+        rating: r.rating,
+        platforms: r.platforms.map((elem) => elem).join(' - '),
+      };
+      res.send(infoDb);
+    });
+  }
 });
 
 router.get('/genres', async (req, res) => {
-	let genres = await axios.get(`https://api.rawg.io/api/genres?key=${APIKEY}`);
-	let genresList = genres.data.results.map(elem => elem.name); // Just mapping out the names of genres
-	let dbFind = await Genre.findAll(); // query to our Genres table
-	let mappedGenres = genres.data.results.map(elem => {
-		// mapping out objs from the api as name:genre_name in order to send them as first response
-		return {
-			name: elem.name,
-		};
-	});
+  let genres = await axios.get(`https://api.rawg.io/api/genres?key=${APIKEY}`);
+  let genresList = genres.data.results.map((elem) => elem.name); // Just mapping out the names of genres
+  let dbFind = await Genre.findAll(); // query to our Genres table
+  let mappedGenres = genres.data.results.map((elem) => {
+    // mapping out objs from the api as name:genre_name in order to send them as first response
+    return {
+      name: elem.name,
+    };
+  });
 
-	if (dbFind.length < 1) {
-		genresList.forEach(elem => Genre.create({ name: elem })); // populate our db's Genres table if no entries found
-		res.send(mappedGenres); // sending first response directly as mapped results from the api
-	} else {
-		res.send(dbFind); // if entries found in our db send them instead
-	}
+  if (dbFind.length < 1) {
+    genresList.forEach((elem) => Genre.create({ name: elem })); // populate our db's Genres table if no entries found
+    res.send(mappedGenres); // sending first response directly as mapped results from the api
+  } else {
+    res.send(dbFind); // if entries found in our db send them instead
+  }
 });
 
 router.post('/videogame', async (req, res) => {
-	let { name, description, released, rating, genres, platforms, image } =
-		req.body; // destructuring the request's body expected params
-	let nuVideo = await Videogame.create({
-		name, // create a new object in db using body params
-		description,
-		released,
-		rating,
-		image,
-		platforms,
-	});
+  let { name, description, released, rating, genres, platforms, image } =
+    req.body; // destructuring the request's body expected params
+  let nuVideo = await Videogame.create({
+    name, // create a new object in db using body params
+    description,
+    released,
+    rating,
+    image,
+    platforms,
+  });
 
-	genres.forEach(async genre => {
-		// look for the req.body genre in db, Genre table
-		let gen = await Genre.findOne({ where: { name: genre } });
-		nuVideo.addGenre(gen); // where it matches add genre to newly created game entry
-	});
-	res.send('Game created');
+  genres.forEach(async (genre) => {
+    // look for the req.body genre in db, Genre table
+    let gen = await Genre.findOne({ where: { name: genre } });
+    nuVideo.addGenre(gen); // where it matches add genre to newly created game entry
+  });
+  res.send('Game created');
 });
 
 module.exports = router;
