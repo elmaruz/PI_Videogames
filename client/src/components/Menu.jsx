14218c183cd38cf9a9bbd2c9a2e25da149dd7d19
elmaruz <lmarussig@gmail.com>
2022-01-28 11:03:03 +0000

    28.01 menu re-styled

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -3,6 +3,8 @@ import SortAZ from './SortAZ';
 import SortRating from './SortRating';
 import FilterDbMobile from './FilterDbMobile';
 import FilterGenMobile from './FilterGenMobile';
+import { IoIosArrowDropright } from 'react-icons/io';
+import { useState } from 'react';
 
 export default function Menu({
   sort,
@@ -14,30 +16,75 @@ export default function Menu({
   onFilterGen,
   genre,
 }) {
+  let [press, setPress] = useState({
+    db: false,
+    genres: false,
+    az: false,
+    ratings: false,
+  });
+
+  const onClickHandler = (e) => {
+    setPress({ ...press, [e]: !press[e] });
+  };
+
   return (
     <div className={s.menu}>
-      <div className={s.section}>
-        <label className={s.lab}>DB</label>
-        <div>
-          <FilterDbMobile db={db} onDb={onDb} />
+      <div className={s.cont}>
+        <div className={s.section}>
+          <div className={s.section_cont}>
+            <label className={s.lab}>DB</label>
+            <div className={press.db ? s.icon_rot : s.icon}>
+              <IoIosArrowDropright
+                onClick={() => onClickHandler('db')}
+                size={20}
+              />
+            </div>
+          </div>
+          <div className={press.db ? s.show : s.hide}>
+            <FilterDbMobile db={db} onDb={onDb} />
+          </div>
         </div>
-      </div>
-      <div className={s.section_gen}>
-        <label>Genres</label>
-        <div className={s.sub_gen}>
-          <FilterGenMobile genre={genre} onFilterGen={onFilterGen} />
+        <div className={s.section}>
+          <div className={s.section_cont}>
+            <label className={s.lab}>Genres</label>
+            <div className={press.genres ? s.icon_rot : s.icon}>
+              <IoIosArrowDropright
+                onClick={() => onClickHandler('genres')}
+                size={20}
+              />
+            </div>
+          </div>
+          <div className={press.genres ? s.show_gen : s.hide}>
+            <FilterGenMobile genre={genre} onFilterGen={onFilterGen} />
+          </div>
         </div>
-      </div>
-      <div className={s.section_sort}>
-        <label className={s.lab}>Sort By Name</label>
-        <div>
-          <SortAZ sort={sort} onSort={onSort} />
+        <div className={s.section}>
+          <div className={s.section_cont}>
+            <label className={s.lab}>A-Z</label>
+            <div className={press.az ? s.icon_rot : s.icon}>
+              <IoIosArrowDropright
+                onClick={() => onClickHandler('az')}
+                size={20}
+              />
+            </div>
+          </div>
+          <div className={press.az ? s.show : s.hide}>
+            <SortAZ sort={sort} onSort={onSort} />
+          </div>
         </div>
-      </div>
-      <div className={s.section_sort}>
-        <label className={s.lab}>Sort By Rating</label>
-        <div>
-          <SortRating ratings={ratings} onRating={onRating} />
+        <div className={s.section}>
+          <div className={s.section_cont}>
+            <label className={s.lab}>Rating</label>
+            <div className={press.ratings ? s.icon_rot : s.icon}>
+              <IoIosArrowDropright
+                onClick={() => onClickHandler('ratings')}
+                size={20}
+              />
+            </div>
+          </div>
+          <div className={press.ratings ? s.show : s.hide}>
+            <SortRating ratings={ratings} onRating={onRating} />
+          </div>
         </div>
       </div>
     </div>
