7f9bd336bb1d33faca08d7cb27ac29859d584621
elmaruz <leonardo@flyp.co>
2026-01-30 15:39:15 +0000

    Follow vercel folder structure convention to fix deploy

--- api/index.js
+++ api/index.js
@@ -1,24 +1,5 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require('../server/src/app.js');
+const { conn } = require('../server/src/db.js');
 
 // Track if database has been synced (resets on cold start)
 let dbSyncPromise = null;
@@ -35,13 +16,3 @@ module.exports = async (req, res) => {
   await syncDatabase();
   return server(req, res);
 };
-
-// Local development
-if (process.env.NODE_ENV !== 'production') {
-  const port = process.env.PORT || 3001;
-  conn.sync({ force: true }).then(() => {
-    server.listen(port, () => {
-      console.log(`Server listening on port ${port}`); // eslint-disable-line no-console
-    });
-  });
-}
